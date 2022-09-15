050534a7e35b09f06cc1b7857344d1024180340c
Pikatan Arya Bramajati <pikatanab26@gmail.com>
2022-09-15 13:43:48 +0700

    Tambah semua lab 2

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
@@ -11,3 +13,15 @@ context = {
     'list_barang': data_barang_wishlist,
     'nama': 'Pikatan Arya Bramajati'
 }
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
